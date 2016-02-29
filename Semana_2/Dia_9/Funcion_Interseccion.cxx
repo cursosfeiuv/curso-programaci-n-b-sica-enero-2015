a3f2bec4779a6cb5abbb7ce42932d17b6d79a670
AlfonsoRamirez <alfonso09.lr@gmail.com>
2016-02-29 12:57:26 -0600

    Recolección de Material Restante del Curso

---
+++ Semana_2/Dia_9/Funcion_Interseccion.cxx
@@ -0,0 +1,22 @@
+Int j =0
+Int I = 0
+	While (j < 3)
+	   I = 0
+	      While  j< 3)
+	      {
+		While (k<3)
+		{
+		   I=0
+		      While (l<3)
+		      {
+			If (a[j][i]==b[k][l]
+			    Cout <<a[j][i];
+			l++,
+		     }
+		K++;
+	      }
+	i++
+      }
+   j++
+  }
+}
